5b9d2d0be4edd48e1b7f15e90d226ee9d9aaf88b
DESKTOP-JCQ9KS6\Kelli_Dev_04 <wlsdus1130@naver.com>
2021-05-27 10:27:28 +0900

    Initial commit

---
+++ android/app/src/main/java/com/example/kakao/kakao_login_test/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.kakao.kakao_login_test;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/login.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+// import kakao sdk
+import 'package:kakao_flutter_sdk/all.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  State createState() {
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State {
+  @override
+  void initState() {
+    super.initState();
+    _initKakaoTalkInstalled();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  _initKakaoTalkInstalled() async {
+    final installed = await isKakaoTalkInstalled();
+    setState(() {
+      _isKakaoTalkInstalled = installed;
+    });
+  }
+
+  bool _isKakaoTalkInstalled = true;
+
+  _issueAccessToken(String authCode) async {
+    try {
+      var token = await AuthApi.instance.issueAccessToken(authCode);
+      AccessTokenStore.instance.toStore(token);
+      Navigator.pushNamed(context, '/login_result');
+    } catch (e) {
+      print("error on issuing access token: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // KaKao native app key
+    KakaoContext.clientId = "27a3806bed74f8f8f8a62b5467ff6c9a";
+    // KaKao javascript key
+    KakaoContext.javascriptClientId = "0db0129dd9d766790df2c3c43252b398";
+
+    isKakaoTalkInstalled();
+
+    return new MaterialApp(
+        home: new Scaffold(
+      appBar: AppBar(
+        title: Text("Kakao Flutter SDK Login"),
+        actions: [],
+      ),
+      body: Center(
+          child: Column(
+            children: [
+              RaisedButton(child: Text("Login"), onPressed: _loginWithKakao),
+              RaisedButton(
+                  child: Text("Login with Talk"),
+                  onPressed: _isKakaoTalkInstalled ? _loginWithTalk : null),
+            ],
+          )),
+      )
+    );
+
+
+  }
+
+  _loginWithKakao() async {
+    try {
+      var code = await AuthCodeClient.instance.request();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  _loginWithTalk() async {
+    try {
+      var code = await AuthCodeClient.instance.requestWithTalk();
+      await _issueAccessToken(code);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
